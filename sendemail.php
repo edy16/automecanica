d83aec5f66040de10af2c3ffffd94d39d57d6b58
Jonas <jlopez90@live.com.mx>
2018-09-19 12:47:16 -0500

    ...

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,8 @@
 
 // Define some constants
 define( "RECIPIENT_NAME", "Auto Mecanica Palenque | Conntacto" );
-define( "RECIPIENT_EMAIL", "jlopez90@live.com.mx" );
+define( "RECIPIENT_EMAIL", "contacto@automecanicapalenque.com" );
+
 
 
 // Read the form values
@@ -10,14 +11,13 @@ $success = false;
 $senderName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
-if ( $senderName && $senderEmail && $phone && $subject && $message) {
+if ( $senderName && $senderEmail && $phone && $message && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
-  $msgBody = " Phone: " . $phone . " Subject: " . $subject . " Message: " . $message . "";
+  $headers = "De: " . $senderName . " <" . $senderEmail . ">";
+  $msgBody = "De: " . $senderName . "\n\nCorreo: " . $senderEmail . "\n\nTelefono: " . $phone . "\n\nMensaje: ". $message;
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successsfull Submission
@@ -26,7 +26,7 @@ if ( $senderName && $senderEmail && $phone && $subject && $message) {
 
 else{
 	//Set Location After Unsuccesssfull Submission
-  	header('Location: index.php?message=Failed');	
+  	header('Location: index.html?message=Failed');	
 }
 
 ?>
